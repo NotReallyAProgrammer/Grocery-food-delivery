dfa382847b6c8ada0160b38eb911f6bfb07d0143
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2023-07-22 05:18:49 +0800

    footer

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { companyData, servicesData } from './footer.data';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  companyData: Array<any> = companyData;
+  serviceData = servicesData;
 }
---
+++ src/app/layouts/footer/footer.data.ts
@@ -0,0 +1,43 @@
+export const companyData = [
+  {
+    routeLink: 'aboutus',
+    label: 'About Us',
+  },
+
+  {
+    routeLink: 'blog',
+    label: 'Blog',
+  },
+
+  {
+    routeLink: 'all-products',
+    label: 'All Products',
+  },
+
+  {
+    routeLink: 'locations-map',
+    label: 'Locations Map',
+  },
+];
+
+export const servicesData = [
+  {
+    routeLink: 'order-tracking',
+    label: 'Order Tracking',
+  },
+
+  {
+    routeLink: 'wish-list',
+    label: 'Wish List',
+  },
+
+  {
+    routeLink: 'my-account',
+    label: 'My Account',
+  },
+
+  {
+    routeLink: 'termsandcondition',
+    label: 'Terms & Condition',
+  },
+];
