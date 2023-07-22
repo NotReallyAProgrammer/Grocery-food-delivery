23afe88b9140dab821361a18686e7502efeba525
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2023-07-23 03:44:58 +0800

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
